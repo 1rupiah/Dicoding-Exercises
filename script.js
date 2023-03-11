da5f696a6586d9190c938c296123c05f8b3069fc
Rahmat Hidayat <rahmat6hidayat@gmail.com>
2023-03-11 10:41:24 +0700

    Added FE1 Task

---
+++ script.js
@@ -0,0 +1,129 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      cards: [
+        {
+          id: 1,
+          title: "Project",
+          desc:
+            "Plan and designate project resources, prepare budgets, monitor progress, and keep stakeholders informed the entire way. This is all done within the confines of a company's goals and vision. Project managers are needed on a wide variety of projects, including construction, IT, HR, and marketing.",
+          photo:
+            "https://images.unsplash.com/photo-1570215171424-f74325192b55?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=880&amp;q=80"
+        },
+        {
+          id: 2,
+          title: "Product",
+          desc:
+            "Who identifies the customer need and the larger business objectives that a product or feature will fulfill, articulates what success looks like for a product, and rallies a team to turn that vision into a reality.",
+          photo:
+            "https://images.unsplash.com/flagged/photo-1550946107-8842ae9426db?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=774&amp;q=80"
+        },
+        {
+          id: 3,
+          title: "Designer",
+          desc:
+            "Developing concepts, graphics and layouts, including making decisions about fonts, images, readability and readers' needs. determining size and arrangement of copy and illustrative material, as well as font style and size. preparing rough drafts of material based on an agreed brief.",
+          photo:
+            "https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=870&amp;q=80"
+        },
+        {
+          id: 4,
+          title: "Geoscientist",
+          desc:
+            "Help protect the planet by studying it, learning from it, and predicting what the future will bring. They perform environmental assessments and study global environmental systems. We locate water, mineral, and energy resources.",
+          photo:
+            "https://images.unsplash.com/photo-1598968693552-39a64f9e24f0?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1001&amp;q=80"
+        }
+      ],
+      currentNum: 0
+    };
+  },
+  computed: {
+    currentCard() {
+      return this.cards[this.currentNum];
+    }
+  },
+  methods: {
+    playFoward() {
+      let tl = gsap.timeline({
+        defaults: {
+          duration: 0.7,
+          ease: "sine.out"
+        },
+        onComplete: () => {
+          this.playReverse();
+          if (this.currentNum >= 3) {
+            this.currentNum = 0;
+          } else {
+            this.currentNum++;
+          }
+        }
+      });
+      tl.to("#mask-1", {
+        yPercent: 100,
+        scaleY: 1.4
+      })
+        .to(
+          "#mask-2",
+          {
+            yPercent: -100,
+            scaleY: 1.4
+          },
+          "<"
+        )
+        .to(
+          "#card-info-title",
+          {
+            clipPath: `polygon(0 0, 100% 0, 100% 0%, 0% 0%)`
+          },
+          "<0.4"
+        )
+        .to(
+          "#card-info-desc",
+          {
+            clipPath: `polygon(0 0, 100% 0, 100% 0%, 0% 0%)`
+          },
+          "<0.3"
+        );
+    },
+    playReverse() {
+      let tl = gsap.timeline({
+        defaults: {
+          duration: 0.7,
+          ease: "sine.in"
+        }
+      });
+      tl.to("#mask-1", {
+        yPercent: -100,
+        scaleY: 1.4
+      })
+        .to(
+          "#mask-2",
+          {
+            yPercent: 100,
+            scaleY: 1.4
+          },
+          "<"
+        )
+        .to(
+          "#card-info-title",
+          {
+            clipPath: `polygon(0 0, 100% 0, 100% 100%, 0% 100%)`
+          },
+          "<0.2"
+        )
+        .to(
+          "#card-info-desc",
+          {
+            clipPath: `polygon(0 0, 100% 0, 100% 100%, 0% 100%)`
+          },
+          "<0.3"
+        );
+    },
+    nextCard() {
+      this.playFoward();
+    }
+  }
+}).mount("#app");
